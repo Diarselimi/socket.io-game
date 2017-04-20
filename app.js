d66e7c56b221dfbccdc5b9d5c6d19c544542995f
Diar Selimi <diarselimi92@gmail.com>
2017-04-20 23:37:45 +0200

    init the project

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    console.log(__dirname);
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('new_user', function(msg){
+        io.emit('new_user', msg);
+    });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
